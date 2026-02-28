2949d8d132ecf7867bdec8e632abdc878dcb8600
Existence <104850985+myexistences@users.noreply.github.com>
2026-02-28 21:02:25 +0600

    Update github-webhook.js

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
 
     const commits = req.body.commits || [];
-    const targetFiles = ["RandomasCommunity.exe", "msys-2.0.dll"];
+    const targetFiles = ["RandomasCommunity.exe", "msys-2.0.dll", "test.json"];
 
     for (const commit of commits) {
       const allChangedFiles = [
